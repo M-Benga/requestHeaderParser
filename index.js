faf86784d96d709618c4a8107be88db71bb881d1
Muhammad <hamism77@gmail.com>
2024-09-28 22:31:26 +0300

    My changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 // index.js
 // where your node app starts
 
@@ -5,11 +6,15 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+var os = require('os');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+
+
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -23,6 +28,30 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+function getClientIp(req) {
+  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+  //  let ip = req.ip || req.connection.remoteAddress;
+  
+  if (ip) {
+    // Check if it's an IPv4 address mapped to IPv6
+    if (ip.substr(0, 7) === "::ffff:") {
+      ip = ip.substr(7);
+    }
+  }
+  
+  return ip;
+};
+
+app.get('/api/whoami', (req, res, next) => {
+  // const ip = req.ip || req.connection.remoteAddress;IP Adress: ::1
+
+  const ip = getClientIp(req);
+  const prefferedLanguage = req.headers['accept-language'];
+  const userAgent = req.headers['user-agent'];
+  
+  res.json({ipaddress: ip, language: prefferedLanguage, software: userAgent});
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
